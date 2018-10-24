44bc20c7918723aeec705f88fa0fe897dbc9404b
Johan Sundström <jhs@apple.com>
2018-10-24 16:12:53 -0700

    style: tidify and show the saved filename

--- snippets/console-save/console-save.js
+++ snippets/console-save/console-save.js
@@ -1,26 +1,23 @@
-(function(console){
-
-    console.save = function(data, filename){
-
-        if(!data) {
-            console.error('Console.save: No data')
-            return;
-        }
-
-        if(!filename) filename = 'console.json'
-
-        if(typeof data === "object"){
-            data = JSON.stringify(data, undefined, 4)
-        }
-
-        var blob = new Blob([data], {type: 'text/json'}),
-            e    = document.createEvent('MouseEvents'),
-            a    = document.createElement('a')
-
-        a.download = filename
-        a.href = window.URL.createObjectURL(blob)
-        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
-        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-        a.dispatchEvent(e)
+((console) => console.save = (data, filename) => {
+    if (!data) {
+        console.error('Console.save: No data!');
+        return;
     }
-})(console)
+
+    if (typeof data === "object") {
+        data = JSON.stringify(data, undefined, 4);
+    }
+
+    const blob = new Blob([data], { type: 'text/json' });
+    const e = document.createEvent('MouseEvents');
+
+    const a = document.createElement('a');
+    a.download = filename = filename || 'console.json';
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl = `text/json:${ a.download }:${ a.href }`;
+
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e);
+
+    console.info(`Saved as: ${filename}`);
+})(console);
